c4b0cd5476f3f4ac2d0320f6eeec5fede0ef2ac4
Ousmane939 <sosukea200@gmail.com>
2024-09-28 03:51:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "✪𝒖𝒔𝒎𝒗𝒏𝒆_🕷️🕸️",
+    ownernumber: process.env.Owner_Number || "22367642669",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
